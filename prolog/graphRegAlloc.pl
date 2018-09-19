74f8b3579fca9825e624385abd7ef464986ee0ef
hsk <sakurai777@gmail.com>
2018-09-19 13:18:35 +0900

    change include predicate to setof predicate

--- prolog/graphRegAlloc.pl
+++ prolog/graphRegAlloc.pl
@@ -5,7 +5,7 @@ adr(A,N) :- m(A:N),!.
 adr(A,N) :- (live(A);live1(A)),retract(c(C)),C1 is C-8,assert(c(C1)),N=ptr(\rbp,C1),assert(m(A:N)).
 adr(_,null).
 adrs(A,A1) :- maplist(adr,A,A1).
-getPush(Cs) :- regp2(Rs),include([R]>>(live(A),m(A:R)),Rs,Cs).
+getPush(Cs) :- regp2(Rs),setof(R,(member(R,Rs),live(A),m(A:R)),Cs);Cs=[].
 code(mov(A,B),mov(A1,B1))           :- adr(A,A1),adr(B,B1).
 code(bin(O,A,B,C),bin(O,A1,B1,C1))  :- adr(A,A1),adr(B,B1),adr(C,C1).
 code(call(A,B,C),call(A,B1,C1,Cs))  :- getPush(Cs),adrs(B,B1),adr(C,C1).
@@ -23,6 +23,6 @@ bb(L:BB,(Lives,BBAlives),L:BB1) :-
   maplist(code1,BB,BBAlives,BB1),retractall(live(_)).
 func(N:_=BBs,(M1,BBsAlives),N:[]=[N1:[enter(-Size,Rs)|Cs]|BBs1]) :-
   assert(c(0)),forall(member(V,M1),assert(m(V))),
-  maplist(bb,BBs,BBsAlives,[N1:Cs|BBs1]),retract(c(Size)),regs2(Regs2),
-  include([R]>>retract(m(_:R)),Regs2,Rs),retractall(m(_)).
+  maplist(bb,BBs,BBsAlives,[N1:Cs|BBs1]),retract(c(Size)),
+  (regs2(Regs2),setof(R,(member(R,Regs2),m(_:R)),Rs);Rs=[]),retractall(m(_)).
 regAlloc(Fs,Fs_) :- dynamic(live/1),alloc(Fs,Allocs),maplist(func,Fs,Allocs,Fs_),!.
