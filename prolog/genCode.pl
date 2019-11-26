bdd73d30219dc13d53151a61a9d6a1dfb903f177
hsk <sakurai777@gmail.com>
2019-11-26 12:06:50 +0900

    added genlabel

--- prolog/genCode.pl
+++ prolog/genCode.pl
@@ -2,8 +2,9 @@
 resetid     :- retractall(id(_)),assert(id(0)).
 genid(S,A)  :- retract(id(C)),C1 is C+1,assert(id(C1)),format(atom(A),'.~w~w',[S,C]).
 
-label(L) :- asserta(l(L)).
-add(C) :- l(L),(c(L,ret(_));assert(c(L,C))).
+genlabel(X,L) :- genid(X,L),assert(l(L)).
+label(L) :- retract(l(L)),asserta(l(L)).
+add(C) :- l(L),assert(c(L,C)).
 get(BBs) :- findall(L:BB,(retract(l(L)),bagof(C,retract(c(L,C)),BB)),R),reverse(R,BBs).
 
 expr(bin(Op,A,B),R) :-  genid(r,R),expr(A,A1),expr(B,B1),add(bin(Op,A1,B1,R)).
@@ -12,15 +13,15 @@ expr(call(A,B),R) :-    genid(r,R),maplist(expr,B,Rs),add(call(A,Rs,R)).
 expr(R,R) :-            atom(R),!.
 expr(I,$I) :-           integer(I),!.
 expr(E,_) :-            throw(genCode(expr(E))).
-stmt(if(A,C,D)) :-      genid(then,Then),genid(else,Else),
-                        expr(A,R1),add(bne(R1,Then,Else)),genid(cont,Cont),
+stmt(if(A,C,D)) :-      genlabel(then,Then),genlabel(else,Else),
+                        expr(A,R1),add(bne(R1,Then,Else)),genlabel(cont,Cont),
                         label(Then),stmt(C),add(br(Cont)),
                         label(Else),stmt(D),add(br(Cont)),label(Cont).
-stmt(while(A,B)) :-     genid(while,While),genid(then,Then),genid(cont,Cont),
+stmt(while(A,B)) :-     genlabel(while,While),genlabel(then,Then),genlabel(cont,Cont),
                         label(While),expr(A,R1),add(bne(R1,Then,Cont)),
                         label(Then),stmt(B),add(br(While)),label(Cont).
 stmt(ret(E)) :-         expr(E,R),add(ret(R)).
 stmt(B) :-              is_list(B),!,forall(member(S,B),stmt(S)).
 stmt(E) :-              expr(E,_).
-func(N:A=B,N:A=BBs) :-  genid(enter,L),label(L),stmt(B),add(ret($0)),get(BBs).
-genCode(P,R) :-         resetid,dynamic(c/2),maplist(func,P,R),!.
+func(N:A=B,N:A=BBs) :-  genlabel(enter,L),label(L),stmt(B),add(ret($0)),get(BBs).
+genCode(P,R) :-         resetid,dynamic(c/2),dynamic(l/2),maplist(func,P,R),!.
--- prolog/liveness.pl
+++ prolog/liveness.pl
@@ -6,10 +6,8 @@ allregs([\rdi,\rsi,\rdx,\rcx,\r8,\r9,\r10,\r11,\rbx,\r12,\r13,\r14,\r15]).
 :- allregs(Rs),findall(R,(between(9,13,I),nth1(I,Rs,R),assert(reg2(R))),Rs_),assert(regs2(Rs_)).
 live_br(G,B,I>O,I2>O2) :- member(B:V,G),member(i=I1,V),union(I1,O,O2),union(I1,I,I2).
 live_code(I>O,I1,I3)   :- union(I,I1,I2),subtract(I2,O,I3).
-live_bb(G,L:[i=I,o=_,bb=BB,br=Br],L:[i=I3,o=O,bb=BB,br=Br]) :-
-    reverse(BB,RBB),(Br=[],[LI>_|_]=RBB -> union(LI,I,IO),O=[]
-                                         ; foldl(live_br(G),Br,I>[],IO>O)),
-    foldl(live_code,RBB,IO,I3).
+live_bb(G,L:[i=_,o=_,bb=BB,br=Br],L:[i=I3,o=O,bb=BB,br=Br]) :-
+  reverse(BB,RBB),foldl(live_br(G),Br,[]>[],IO>O),foldl(live_code,RBB,IO,I3).
 live(G,R) :- maplist(live_bb(G),G,G2),(G=G2->R=G;live(G2,R)).
 
 alive_code(I>O,(Lives,Alives),(Lives2,[(Mks,Rms)|Alives])) :-
